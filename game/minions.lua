a98c540bae136d36b3054952a4eb575a215c5cb9
ftsell <dev@ftsell.de>
2024-04-13 13:55:57 +0200

    add non-walkable fields based on map

--- game/minions.lua
+++ game/minions.lua
@@ -1,6 +1,7 @@
 require("game.state")
 require("game.conf")
 require("game.utils")
+require("game.tilemap")
 
 game.minions = {}
 
@@ -9,24 +10,33 @@ local MOVE_SPEED = game.conf.minions.moveSpeed
 function game.minions.load()
 	game.state.minions = {
 		{ position = { x = 100, y = 200 }, color = { 255, 0, 0 } },
-		{ position = { x = 200, y = 200 }, color = { 0, 255, 0 } },
-		{ position = { x = 300, y = 400 }, color = { 0, 0, 255 } },
+		--{ position = { x = 200, y = 200 }, color = { 0, 255, 0 } },
+		--{ position = { x = 300, y = 400 }, color = { 0, 0, 255 } },
 	}
 end
 
 function game.minions.update(dt)
-	for i, minion in ipairs(game.state.minions) do
+	for _, minion in ipairs(game.state.minions) do
+		-- prepare to move the minion
+		local newX, newY = minion.position.x, minion.position.y
 		if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
-			minion.position.y = utils.clamp(0, minion.position.y - MOVE_SPEED * dt, 600)
+			newY = utils.clamp(0, minion.position.y - MOVE_SPEED * dt, 600)
 		end
 		if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
-			minion.position.y = utils.clamp(0, minion.position.y + MOVE_SPEED * dt, 600)
+			newY = utils.clamp(0, minion.position.y + MOVE_SPEED * dt, 600)
 		end
 		if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
-			minion.position.x = utils.clamp(0, minion.position.x - MOVE_SPEED * dt, 800)
+			newX = utils.clamp(0, minion.position.x - MOVE_SPEED * dt, 800)
 		end
 		if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
-			minion.position.x = utils.clamp(0, minion.position.x + MOVE_SPEED * dt, 800)
+			newX = utils.clamp(0, minion.position.x + MOVE_SPEED * dt, 800)
+		end
+
+		-- check if the new position is valid
+		local mapX, mapY = game.tilemap.screenToWorldPos(newX, newY)
+		if game.tilemap.getValue(mapX, mapY, "walkable") == true then
+			minion.position.x = newX
+			minion.position.y = newY
 		end
 	end
 end
--- main.lua
+++ main.lua
@@ -6,13 +6,11 @@ require("game.tilemap")
 
 function love.load()
 	game.tilemap.load()
-	time = 0
 	game.summoning.load()
 	game.minions.load()
 end
 
 function love.update(dt)
-	time = dt + time
 	game.tilemap.update(dt)
 	game.summoning.update(dt)
 	game.minions.update(dt)

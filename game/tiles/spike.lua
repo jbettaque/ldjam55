a1492b4c1c74f87c5dfab77d0f0d162ccaad184b
pkottsiepe <pkottsiepe@users.noreply.github.com>
2024-04-13 20:25:50 +0000

    fix pre-commit warnings

--- game/tiles/spike.lua
+++ game/tiles/spike.lua
@@ -3,15 +3,15 @@ game.tiles.spike = {}
 local asset = "assets/tiles/tile_spike.png"
 
 function game.tiles.spike.register()
-    local assetOffset = game.tilemap.getAssetCount()
+	local assetOffset = game.tilemap.getAssetCount()
 
-    game.tilemap.registerAsset(asset)
-    print("assetOffset: " .. assetOffset)
+	game.tilemap.registerAsset(asset)
+	print("assetOffset: " .. assetOffset)
 
-    game.tiles.spike.tilePreset = {
-        asset = 1 + assetOffset,
-        walkable = false,
-    }
+	game.tiles.spike.tilePreset = {
+		asset = 1 + assetOffset,
+		walkable = false,
+	}
 
-    game.tilemap.registerTilePreset("spike", game.tiles.spike.tilePreset)
+	game.tilemap.registerTilePreset("spike", game.tiles.spike.tilePreset)
 end

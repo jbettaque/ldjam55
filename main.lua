124864a09cdddfa753b8a602e50878710d40f58c
Jo <bettaque@gmail.com>
2024-04-13 10:00:48 +0200

    example project

---
+++ main.lua
@@ -0,0 +1,13 @@
+function love.load()
+    time = 0
+end
+function love.update(dt)
+    time = dt + time
+end
+function love.draw()
+    love.graphics.print("Hello World " .. time, 400, 250 )
+end
+
+
+
+

dcdcf87b00744fa95ea9fd47a987c7d518725287
ftsell <dev@ftsell.de>
2024-04-13 13:30:56 +0200

    add support for multiple minions

--- game/minions.lua
+++ game/minions.lua
@@ -7,30 +7,33 @@ game.minions = {}
 local MOVE_SPEED = game.conf.minions.moveSpeed
 
 function game.minions.load()
-	game.state.minions[1] = {
-		position = {
-			x = 400,
-			y = 300,
-		},
+	game.state.minions = {
+		{ position = { x = 100, y = 200 }, color = { 255, 0, 0 } },
+		{ position = { x = 200, y = 200 }, color = { 0, 255, 0 } },
+		{ position = { x = 300, y = 400 }, color = { 0, 0, 255 } },
 	}
 end
 
 function game.minions.update(dt)
-	if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
-		game.state.minions[1].position.y = utils.clamp(0, game.state.minions[1].position.y - MOVE_SPEED * dt, 600)
-	end
-	if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
-		game.state.minions[1].position.y = utils.clamp(0, game.state.minions[1].position.y + MOVE_SPEED * dt, 600)
-	end
-	if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
-		game.state.minions[1].position.x = utils.clamp(0, game.state.minions[1].position.x - MOVE_SPEED * dt, 800)
-	end
-	if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
-		game.state.minions[1].position.x = utils.clamp(0, game.state.minions[1].position.x + MOVE_SPEED * dt, 800)
+	for i, minion in ipairs(game.state.minions) do
+		if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
+			minion.position.y = utils.clamp(0, minion.position.y - MOVE_SPEED * dt, 600)
+		end
+		if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
+			minion.position.y = utils.clamp(0, minion.position.y + MOVE_SPEED * dt, 600)
+		end
+		if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+			minion.position.x = utils.clamp(0, minion.position.x - MOVE_SPEED * dt, 800)
+		end
+		if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+			minion.position.x = utils.clamp(0, minion.position.x + MOVE_SPEED * dt, 800)
+		end
 	end
 end
 
 function game.minions.draw()
-	love.graphics.setColor(255, 0, 0)
-	love.graphics.circle("fill", game.state.minions[1].position.x, game.state.minions[1].position.y, 10)
+	for i, minion in ipairs(game.state.minions) do
+		love.graphics.setColor(love.math.colorFromBytes(unpack(minion.color)))
+		love.graphics.circle("fill", minion.position.x, minion.position.y, 10)
+	end
 end

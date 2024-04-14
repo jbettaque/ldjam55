176d586c02d11d66471460502e639760f765842d
Jo <bettaque@gmail.com>
2024-04-14 14:55:24 +0200

    implemented explody mines

--- game/tiles/mine.lua
+++ game/tiles/mine.lua
@@ -2,6 +2,12 @@ game.tiles.mine = {}
 
 local asset = "assets/tiles/tile_mine.png"
 
+mine_step_func = function(x, y, minion)
+	print("step on mine")
+	game.minions.kill(minion)
+	game.tilemap.setTileWithPreset(x, y, tilePresets["ground"])
+end
+
 function game.tiles.mine.register()
 	local assetOffset = game.tilemap.getAssetCount()
 
@@ -11,9 +17,11 @@ function game.tiles.mine.register()
 	game.tiles.mine.tilePreset = {
 		preset = "mine",
 		asset = 1 + assetOffset,
-		walkable = false,
-		overFlyable = false,
+		walkable = true,
+		overFlyable = true,
+		step_on = "mine_step_func",
 	}
 
 	game.tilemap.registerTilePreset("mine", game.tiles.mine.tilePreset)
+	game.tilemap.registerStepOnFunction("mine_step_func", mine_step_func)
 end

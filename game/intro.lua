dfd1919a09ac6b7a1856c9cb92f9da5c25636204
Ole Bittner <dev@oleb.it>
2024-04-14 19:38:12 +0200

    add some text

--- game/intro.lua
+++ game/intro.lua
@@ -30,7 +30,7 @@ local function drawContainer(title)
 	local titleHeight = game.conf.ui.intro.titleHeight
 	love.graphics.setColor(1, 1, 1)
 	love.graphics.setNewFont(titleHeight * 0.8)
-	love.graphics.printf("Welcome to the Game", x1 + border, y1 + border, containerWidth, "center")
+	love.graphics.printf(title, x1 + border, y1 + border, containerWidth, "center")
 	love.graphics.line(x1, y1 + border + titleHeight + border, x1 + containerWidth, y1 + border + titleHeight + border)
 
 	--- Print closing remark
@@ -80,9 +80,64 @@ local function isInFooter(x, y)
 		and y <= containerY + containerHeight
 end
 
-local function drawLevel1()
+local function drawLevelSummoning()
 	x, y, width, height = drawContainer("Welcome to the game")
-	love.graphics.printf("Hello World", x, y, width, "left")
+	love.graphics.printf("Hello World.", x, y, width, "left") -- TODO write intro and explain controls
+end
+
+local function drawLevelInteractions()
+	x, y, width, height = drawContainer("Interactions")
+	love.graphics.printf(
+		"Your minions can interact with various objects in your chambers. In this room they need to operate a switch and a pressure plate.",
+		x,
+		y,
+		width,
+		"left"
+	)
+end
+
+local function drawLevelDifferentSpeeds()
+	x, y, width, height = drawContainer("Different Speeds")
+	love.graphics.printf(
+		"Naturally you don't only have homunculi at your command. Summon a zombie to clear the next room. As you know zombies are slower than your other minions and they are also dumber and can't interact with object. However, they are ideal as ballast for pressure plates.",
+		x,
+		y,
+		width,
+		"left"
+	)
+end
+
+local function drawLevelYouCanDie()
+	x, y, width, height = drawContainer("Minions are mortal")
+	love.graphics.printf(
+		"As you are well aware your minions can die, from running brainlessly into spikes for example. But don't be alarmed deceased minions can be re-summoned immediately.",
+		x,
+		y,
+		width,
+		"left"
+	)
+end
+
+local function drawLevelYouCanFly()
+	x, y, width, height = drawContainer("Fly Away")
+	love.graphics.printf(
+		"The next room contains pits and spikes which cannot be avoided. I suggest you summon a fae, which can elegantly glide above them.",
+		x,
+		y,
+		width,
+		"left"
+	)
+end
+
+local function drawLevelDontFlyTooHigh()
+	x, y, width, height = drawContainer("Don't fly too high")
+	love.graphics.printf(
+		"Fae might be able to fly above dangers and obstacles. But as you certainly know, this also prevents them from pressing down pressure plates.",
+		x,
+		y,
+		width,
+		"left"
+	)
 end
 
 --- callback when the game starts
@@ -117,9 +172,20 @@ end
 
 --- callback to draw on the screen
 function game.intro.draw()
+	local lvl = game.state.level.current
 	if state.isOpen then
-		if game.state.level.current == 1 then
-			drawLevel1()
+		if lvl == 2 then
+			drawLevelSummoning()
+		elseif lvl == 3 then
+			drawLevelInteractions()
+		elseif lvl == 4 then
+			drawLevelDifferentSpeeds()
+		elseif lvl == 6 then
+			drawLevelYouCanDie()
+		elseif lvl == 8 then
+			drawLevelYouCanFly()
+		elseif lvl == 9 then
+			drawLevelDontFlyTooHigh()
 		else
 			game.intro.close()
 		end
